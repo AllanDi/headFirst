b975827eb87d9b111852f552cda74e52b9399862
Allan <allandrs@gmail.com>
2024-01-07 12:11:50 -0300

    starting chapter two first object exercice

---
+++ src/chapterTwo/firstObject/Dog.java
@@ -0,0 +1,13 @@
+package chapterTwo.firstObject;
+
+public class Dog {
+
+    int size;
+    String breed;
+    String name;
+
+    void bark(){
+        System.out.println("Ruff! Ruff!");
+    }
+
+}
---
+++ src/chapterTwo/firstObject/DogTestDrive.java
@@ -0,0 +1,16 @@
+package chapterTwo.firstObject;
+
+public class DogTestDrive {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.size = 40;
+
+        d.bark();
+
+
+    }
+
+}
