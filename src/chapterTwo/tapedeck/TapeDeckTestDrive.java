a7dab98da0164194c66aa759ee903432a7823ef4
Allan <allandrs@gmail.com>
2024-01-17 07:48:18 -0300

    wip be compiler chapter two exercices

---
+++ src/chapterTwo/tapedeck/TapeDeckTestDrive.java
@@ -0,0 +1,16 @@
+package chapterTwo.tapedeck;
+
+public class TapeDeckTestDrive {
+
+    public static void main(String[] args) {
+
+        TapeDeck t = new TapeDeck();
+
+        t.canRecord = true;
+        t.playTape();
+
+        if(t.canRecord == true){
+            t.recordTape();
+        }
+    }
+}
