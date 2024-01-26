a022a52dbe1a189e1cd6e42fa54662dc8560bc7a
Allan <allandrs@gmail.com>
2024-01-26 08:32:50 -0300

    wip chapter three

---
+++ src/chapterThree/ObjectReference.java
@@ -0,0 +1,64 @@
+package chapterThree;
+public class ObjectReference {
+        public static void main(String[] args) {
+
+//        Book b = new Book();
+//        Book c = new Book();
+//        Book d = c;
+//
+//
+//        b.name = "Livro (B)";
+//        c.name = "Livro (C)";
+//        d.name = "Livro (D)";
+//
+//        System.out.println(b.name);
+//        System.out.println(c.name);
+//        System.out.println(d.name);
+//
+//            System.out.println("Fazendo as atribuições testes: ");
+//
+//
+//        System.out.println(b.name);
+//        System.out.println(c.name);
+//        System.out.println(d.name);
+
+
+
+            int[] nums;
+            nums = new int[7];
+            nums[0] = 6;
+            nums[1] = 19;
+            nums[2] = 44;
+            nums[3] = 42;
+            nums[4] = 10;
+            nums[5] = 20;
+            nums[6] = 1;
+
+            System.out.println(nums[5]);
+
+            Dog[] pets = new Dog[7];
+            pets[0] = new Dog("rex");
+
+            System.out.println(pets[0].name);
+
+    }
+
+
+
+
+
+}
+class Book {
+    int pages;
+    String name;
+
+}
+
+class Dog{
+    String name;
+
+    public Dog(String name){
+        this.name = name;
+    }
+
+}
