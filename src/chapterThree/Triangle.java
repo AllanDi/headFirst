344656fbdf3c91a0e5d59eddced6ebe940ac177c
Allan <allandrs@gmail.com>
2024-01-31 07:37:34 -0300

    wip chapter three

--- src/chapterThree/Triangle.java
+++ src/chapterThree/Triangle.java
@@ -6,9 +6,7 @@ public class Triangle {
     int height;
     int length;
 
-
     public static void main(String[] args) {
-
         Triangle[] ta = new Triangle[4];
 
         ta[0] = new Triangle();
@@ -16,55 +14,28 @@ public class Triangle {
         ta[2] = new Triangle();
         ta[3] = new Triangle();
 
-
-
-
-
         int x = 0;
-        int y = x;
+        int y = 0;
 
-
-
-
-        while (x < 4){
-
-
-
+        while (x < 4) {
             ta[x].height = (x + 1) * 2;
             ta[x].length = x + 4;
             ta[x].setArea();
 
-
-
             System.out.print("triangle " + x + ", area");
-            System.out.println(" = "+ ta.area);
+            System.out.println(" = " + ta[x].area);
 
             x++;
-
         }
 
-
-        x = 27;
+        y = x;
 
         Triangle t5 = ta[2];
-        ta[2].area = 343;
-
         System.out.print("y = " + y);
         System.out.println(", t5 area = " + t5.area);
-
     }
 
-    void setArea(){
-
-           area = (height * length) / 2;
-
+    void setArea() {
+        area = (height * length) / 2.0;
     }
-
-
 }
-
-//triangle 0, area = 4.0
-//triangle 1, area = 10.0
-//triangle 2, area = 18.0
-//triangle 3, area =______
-//y=________________
