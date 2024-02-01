596ed37e9fdd97289b8479334264643839f685ed
Allan <allandrs@gmail.com>
2024-02-01 08:03:53 -0300

    wip chapter three

---
+++ src/chapterFour/Test.java
@@ -0,0 +1,20 @@
+package chapterFour;
+
+public class Test {
+
+    static int x = 7;
+    void go(int z){
+
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+
+        test.go(x);
+    }
+
+
+
+
+
+}
