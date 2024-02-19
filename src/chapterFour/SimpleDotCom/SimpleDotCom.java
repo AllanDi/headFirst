2e4ebdab2dc50b5027ef16b29ce4b01b8bd33ac6
Allan <allandrs@gmail.com>
2024-02-18 21:03:56 -0300

    wip dotCom game

---
+++ src/chapterFour/SimpleDotCom/SimpleDotCom.java
@@ -0,0 +1,19 @@
+package chapterFour.SimpleDotCom;
+
+public class SimpleDotCom {
+
+    -->declarar uma matriz de inteiros para armazenas o local das celulas, (locationCells)
+    -->declarar um inteiro que armazena o numero de palpites (numOfHits) iniciando em 0
+
+    -->declarar um metodo (checkYourSelf que use a string para o palpite do usuario, verificar a string e
+                        retornar (acerto), (erro) ou eliminado
+
+    -->declarar um metodo de configuração (setLocationCells) que use a matriz int contendo os tres locais
+    das celulas na forma de numeros inteiros gerados aleatiamentes
+
+
+
+
+
+
+}
---
+++ src/chapterFour/SimpleDotCom/SimpleDotComGame.java
@@ -0,0 +1,31 @@
+package chapterFour.SimpleDotCom;
+
+public class SimpleDotComGame {
+
+    public static void main(String[] args) {
+
+        --> metodo checkTourSelf
+            pegue o palpite do usuario como string
+            converter o palpite do usuario como um int
+
+                repita -->em cada local da celula
+                        valide se o palpite do usuario esta correto
+                se o palpite do usuario estiver correto
+                        aumente o numero de acerto
+                        verificar se foi a ultima celula
+                            se o numero de acertos for igual a 3
+                            retorne eliminação como resultado
+                            caso contrario não tera eliminado, retorne "correto"
+                caso contrario o palpite esta incorreto, retorne errado
+
+            fim da iteracao
+
+
+
+
+
+
+
+    }
+
+}
